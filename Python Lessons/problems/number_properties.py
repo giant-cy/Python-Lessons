e78e4e20a7d464f01f6ac4b5ffb08a201272b1a1
giant-cy <84373077+giant-cy@users.noreply.github.com>
2026-03-17 17:18:10 +0200

    Update number_properties.py

--- Python Lessons/problems/number_properties.py
+++ Python Lessons/problems/number_properties.py
@@ -1,15 +1,15 @@
-# # 7.
-# def is_prime(number):
-#     checker = int(number ** 0.5)
-#     for i in range(2, checker+1):
-#         if number % i == 0:
-#             print(f"{number}, is not a prime")
-#             return
-#     print(f"{number}, is a prime")
-# is_prime(20)
-# is_prime(57)
-# is_prime(73)
-# is_prime(17)
+# 7.
+def is_prime(number):
+    checker = int(number ** 0.5)
+    for i in range(2, checker+1):
+        if number % i == 0:
+            print(f"{number}, is not a prime")
+            return
+    print(f"{number}, is a prime")
+is_prime(20)
+is_prime(57)
+is_prime(73)
+is_prime(17)
 
 # 8.
 def primes(number):
