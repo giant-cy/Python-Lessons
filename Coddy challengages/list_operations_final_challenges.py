0949d0ed86aa70d464796b87093de28c4ac83dbf
giant-cy <psixas@gmail.com>
2026-04-23 01:19:07 +0300

    fix transpose

--- Coddy challengages/list_operations_final_challenges.py
+++ Coddy challengages/list_operations_final_challenges.py
@@ -1,52 +1,34 @@
-
-
-
-lst = [
-	[1, 2],
-	[4, 5],
-	[7, 8]
-]
-
-
-# [
-# 	[1, 4, 7],
-# 	[2, 5, 8]
-# ]
-
-def transpose(lst):
-    columns = len(lst) #3
-    rows = len(lst[0]) #2
+def transpose(input_list):
+    columns = len(input_list)  # number of rows in original = 3
+    rows = len(input_list[0])  # number of columns in original = 2
     new_list = []
-    for i in range(rows):
+
+    for i in range(rows):  # i = 0, 1
         new_list.append([])
-        # for j in range(columns):
+        for j in range(columns):  # j = 0, 1, 2
+            new_list[i].append(input_list[j][i])
 
-    print(new_list, sep= '\n')
+    print(new_list)
 
-
-
-# lst = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
+list_1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# [ output
+# 	[1, 4, 7],
+# 	[2, 5, 8],
+# 	[3, 6, 9]
 # ]
-# transpose(lst)
-#
-# # [
-# # 	[1, 4, 7],
-# # 	[2, 5, 8],
-# # 	[3, 6, 9]
-# # ]
+transpose(list_1)
 
-lst_2 = [
+list_2 = [
 	[1, 2],
 	[4, 5],
 	[7, 8]
 ]
-transpose(lst_2)
-
-
-# [
+# [ output
 # 	[1, 4, 7],
 # 	[2, 5, 8]
 # ]
+transpose(list_2)
